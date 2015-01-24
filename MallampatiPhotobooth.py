38672ea2f342c2e426541f3d9df8e64fb31bcd01
Raoul <raoul.schorer@gmail.com>
2015-01-24 22:04:55 +0100

    webcam photobooth removed for android and iOS
    Introduced a Jinja condition in the photobooth page template to not
    display the webcam photobooth on iOS & android devices.

--- MallampatiPhotobooth.py
+++ MallampatiPhotobooth.py
@@ -1,5 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
+#get user agent
+def os_platform_not_desktop():
+    '''
+    uses werkzeug classes to detect OS. I used that because I do not
+    want android & iOS devices to get the default photobooth.
+    Use browser capability detection such as modernizer for anything else.
+    '''
+    platform = request.user_agent.platform
+    if (platform == 'android') or (platform == 'ipad') or (platform == 'iphone'):
+        return True
+    else:
+        return False
+
+#define flask routes
 app = Flask(__name__)
 
 @app.route('/')
@@ -8,7 +22,8 @@ def index():
 
 @app.route('/photobooth')
 def photobooth():
-    return render_template('photobooth.html')
+    is_mobile = os_platform_not_desktop()
+    return render_template('photobooth.html', is_mobile = is_mobile)
 
 if __name__ == '__main__':
 	app.config["SECRET_KEY"] = "RaoulKnowsNothingBetter"
