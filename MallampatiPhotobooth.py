135fb1cc29455cc06b9817ba1a4891679d2fc82f
Raoul <raoul.schorer@gmail.com>
2015-01-25 02:13:52 +0100

    deactivate custom.je on mobile

--- MallampatiPhotobooth.py
+++ MallampatiPhotobooth.py
@@ -1,17 +1,5 @@
-from flask import Flask, render_template, request
-
-#get user agent
-def os_platform_not_desktop():
-    '''
-    uses werkzeug classes to detect OS. I used that because I do not
-    want android & iOS devices to get the default photobooth.
-    Use browser capability detection such as modernizer for anything else.
-    '''
-    platform = request.user_agent.platform
-    if (platform == 'android') or (platform == 'ipad') or (platform == 'iphone'):
-        return True
-    else:
-        return False
+from flask import Flask, render_template
+from detect_platform import detect_platform
 
 #define flask routes
 app = Flask(__name__)
@@ -22,7 +10,7 @@ def index():
 
 @app.route('/photobooth')
 def photobooth():
-    is_mobile = os_platform_not_desktop()
+    is_mobile = detect_platform.os_platform_not_desktop()
     return render_template('photobooth.html', is_mobile = is_mobile)
 
 if __name__ == '__main__':
--- test/client_test.py
+++ test/client_test.py
@@ -8,8 +8,9 @@ Created on Fri Jan 16 21:38:31 2015
 import unittest
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
+from detect_platform import detect_platform
 
-class MallampatiTests(unittest.TestCase):
+class HomepageTests(unittest.TestCase):
 
     def setUp(self):
         self.driver = webdriver.Firefox()
@@ -19,17 +20,33 @@ class MallampatiTests(unittest.TestCase):
         self.assertIn("Mallampati", self.driver.title)
         
     def test_page_header(self):
-        content = self.driver.find_element_by_class_name("jumbotron")
-        self.assertIn("Welcome", content.text)
+        self.content = self.driver.find_element_by_class_name("jumbotron")
+        self.assertIn("Welcome", self.content.text)
+        
+    def tearDown(self):
+        self.driver.close()
+        
+
+class PhotoboothpageTests(unittest.TestCase):
+    
+    def setUp(self):
+        self.driver = webdriver.Firefox()
+        self.driver.get("http://127.0.0.1:8051/photobooth")
         
     def test_form_exists(self):
-        self.driver.get("http://127.0.0.1:8051/photobooth")
         try:
             self.form = self.driver.find_element_by_class_name("form-group")
             self.exists = True
         except NoSuchElementException:
             self.exists = False
         self.assertTrue(self.exists)
+        
+    def test_webcamphotobooth_if_desktop(self):
+        from MallampatiPhotobooth import app
+        with app.test_request_context():  # required to get context locals
+            self.is_mobile = detect_platform.os_platform_not_desktop()
+            if self.is_mobile == False:
+                self.assertTrue(self.driver.find_element_by_id("my_camera"))
 
     def tearDown(self):
         self.driver.close()
